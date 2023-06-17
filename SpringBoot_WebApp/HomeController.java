e3088cc70647e8fb9520ed0676afe0aa1602034d
Paurush <40339719+paurush1710@users.noreply.github.com>
2023-06-17 17:29:58 +0530

    Create HomeController.java

---
+++ SpringBoot_WebApp/HomeController.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+	
+//	@RequestMapping("home")
+//	public String home(@RequestParam("name")String myName, HttpSession session) {
+//		System.out.println("Hi : "+ myName);
+//		
+//		session.setAttribute("name", myName);
+//		
+//		return "home.jsp";
+//	}
+	
+//	@RequestMapping("home")
+//	public ModelAndView home(@RequestParam("aname")String myName) {
+//		
+//		ModelAndView mv = new ModelAndView();
+//		System.out.println("Hi : "+ myName);	
+//		mv.setViewName("home.jsp");
+//		mv.addObject("name", myName);
+//		return mv;
+//	}
+	
+	@RequestMapping("home")
+	public ModelAndView home(Alien alien) {
+		
+		ModelAndView mv = new ModelAndView();
+		System.out.println("Hi : "+ alien.getAname());
+		mv.setViewName("home.jsp");
+		mv.addObject("obj", alien);
+		return mv;
+	}
+	
+	// Output - WELCOME 1 , Jakab , Java
+	
+}
